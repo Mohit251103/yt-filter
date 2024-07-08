af8fa6d4cdfe36f3036e0a7d225a752a7b1eb090
Mohit251103 <142974436+Mohit251103@users.noreply.github.com>
2024-07-08 15:22:24 +0530

    initialized

---
+++ youtube-detox/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='flex justify-center items-center h-[100vh] w-[100vw]'>
+            <div className='h-[40%] w-[40%] bg-white'>
+                <p className='text-[blue] text-center font-bold text-2xl'>Login</p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
