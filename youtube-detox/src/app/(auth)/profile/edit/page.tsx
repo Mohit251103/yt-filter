39f2d04f2851fdd49654158c347e230391bbb68b
Mohit251103 <142974436+Mohit251103@users.noreply.github.com>
2024-08-30 22:29:55 +0530

    Changes

--- youtube-detox/src/app/(auth)/profile/edit/page.tsx
+++ youtube-detox/src/app/(auth)/profile/edit/page.tsx
@@ -43,33 +43,35 @@ const Profile = () => {
 
         return (
             <div className='h-[100vh] bg-white flex flex-col justify-center items-center'>
-                (!loading?<Brightness4Icon className='absolute top-0 right-0 -translate-x-5 translate-y-5'></Brightness4Icon>
-                <div>
-                    <img src="/filtered2.png" alt="" />
-                </div>
-                <div className='font-bold'>
-                    <p className='text-3xl '>Welcome to <span className='text-red-500'>filtered.</span></p>
-                    <p className='text-xl'>The only solution to a filtered feed space</p>
-                </div>
-                <div className='h-fit w-[25%] border border-2 m-5 rounded-2xl p-3'>
-                    <p className='text-xl text-center'>Create a username</p>
-                    <form onSubmit={handleSubmit(changeUsername)} className='flex flex-col my-3'>
-                        <TextField {...register('username')} id="outlined-basic" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} value={username} />
-                        {errors.username?.message && <p className='text-sm text-[red]'>{errors.username?.message.toString()}</p>}
-                        <Button type="submit" variant='outlined' className='w-1/4 my-2'>Proceed</Button>
-                    </form>
-                </div>:
-                <ThreeDots
-                    visible={true}
-                    height="80"
-                    width="80"
-                    color="black"
-                    radius="9"
-                    ariaLabel="three-dots-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                />
-                )
+                {!loading ?
+                    <>
+                        <div>
+                            <img src="/filtered2.png" alt="" />
+                        </div>
+                        <div className='font-bold'>
+                            <p className='text-3xl '>Welcome to <span className='text-red-500'>filtered.</span></p>
+                            <p className='text-xl'>The only solution to a filtered feed space</p>
+                        </div>
+                        <div className='h-fit w-[25%] border border-2 m-5 rounded-2xl p-3'>
+                            <p className='text-xl text-center'>Create a username</p>
+                            <form onSubmit={handleSubmit(changeUsername)} className='flex flex-col my-3'>
+                                <TextField {...register('username')} id="outlined-basic" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} value={username} />
+                                {errors.username?.message && <p className='text-sm text-[red]'>{errors.username?.message.toString()}</p>}
+                                <Button type="submit" variant='outlined' className='w-1/4 my-2'>Proceed</Button>
+                            </form>
+                        </div>
+                    </> :
+                    <ThreeDots
+                        visible={true}
+                        height="80"
+                        width="80"
+                        color="black"
+                        radius="9"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                    />
+                }
             </div>
         )
     }
